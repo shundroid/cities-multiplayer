20e8f641d805d8a45efca9e4502d7403b77b8a9b
shundroid <shundroidk@gmail.com>
2017-01-24 18:11:46 +0900

    google drive hello

--- Google Drive Test/Program.cs
+++ Google Drive Test/Program.cs
@@ -1,15 +1,69 @@
-﻿using System;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
 
 namespace Google_Drive_Test
 {
   class Program
   {
+    // If modifying these scopes, delete your previously saved credentials
+    // at ~/.credentials/drive-dotnet-quickstart.json
+    static string[] Scopes = { DriveService.Scope.DriveReadonly };
+    static string ApplicationName = "Drive API .NET Quickstart";
+
     static void Main(string[] args)
     {
+      UserCredential credential;
+
+      using (var stream =
+          new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+      {
+        string credPath = Environment.GetFolderPath(
+            Environment.SpecialFolder.Personal);
+        credPath = Path.Combine(credPath, ".credentials/drive-dotnet-quickstart.json");
+
+        credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+            GoogleClientSecrets.Load(stream).Secrets,
+            Scopes,
+            "user",
+            CancellationToken.None,
+            new FileDataStore(credPath, true)).Result;
+        Console.WriteLine("Credential file saved to: " + credPath);
+      }
+
+      // Create Drive API service.
+      var service = new DriveService(new BaseClientService.Initializer()
+      {
+        HttpClientInitializer = credential,
+        ApplicationName = ApplicationName,
+      });
+
+      // Define parameters of request.
+      FilesResource.ListRequest listRequest = service.Files.List();
+      listRequest.PageSize = 10;
+      listRequest.Fields = "nextPageToken, files(id, name)";
+
+      // List files.
+      IList<Google.Apis.Drive.v3.Data.File> files = listRequest.Execute()
+          .Files;
+      Console.WriteLine("Files:");
+      if (files != null && files.Count > 0)
+      {
+        foreach (var file in files)
+        {
+          Console.WriteLine("{0} ({1})", file.Name, file.Id);
+        }
+      }
+      else
+      {
+        Console.WriteLine("No files found.");
+      }
+      Console.Read();
     }
   }
 }
